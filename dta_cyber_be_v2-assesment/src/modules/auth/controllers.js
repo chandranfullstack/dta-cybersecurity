73fac154dc418ac6d34acee46893bae352278ea5
chandranfullstack <chandran@fulgid.in>
2023-03-25 13:56:19 +0530

    K

--- dta_cyber_be_v2-assesment/src/modules/auth/controllers.js
+++ dta_cyber_be_v2-assesment/src/modules/auth/controllers.js
@@ -12,68 +12,6 @@ const loginResponse1 = (user) => ({
 
 
 // Login api for the app. Sends token.
-// const loginController = async (req, res, next, ...rest) => {
-//     class _Validator extends BaseApiValidator {
-//         fields = {
-//             "password": {},
-//             "username": {},
-//         }
-//         async validate_username(value, data) {
-//             let user = await User.findOne({
-//                 where: {
-//                     username: value,
-//                 }
-//             })
-//             console.log(isNull(user),user)
-//             // skip ldap check for users
-//             if (value === 'admin' && isNull(user)) {
-//                 console.log(user,"val")
-//                 res.json(200, {
-//                     data,
-//                     is_success: true,
-//                     action: "DO_NOTHING"
-//                 })
-//             }
-
-//             // LDAP Try
-//             let ldapResponse = await ldapAuthenticate(
-//                 value, data['password']
-//             )
-
-//             // LDAP
-//             if (ldapResponse['isSuccess'] === false) {
-//                 throw new ApiError("Unable to login with provided credentials.")
-//             }
-
-//             // Handle LDAP Auth
-//             if (!isNull(ldapResponse) && ldapResponse['isSuccess'] === true && isNull(user)) {
-//                 user = await User.create({
-//                     username: value,
-//                     password: null
-//                 })
-//             }
-
-//             // TODO: Login Completion with Daimler Integration
-//             // TODO: GraphQL + LDAP
-//             return user
-//         }
-//     }
-
-//     try {
-//         const validator = new _Validator(req)
-//         console.log(await validator.isValid(),"isValid")
-//         await validator.isValid()
-//         console.log(await validator.isValid())
-//         const user = validator.data['username']
-//         loginResponse1(user)
-//         console.log(user)
-//         apiOkResponse(res, loginResponse1(user))
-
-//     } catch (e) {
-//         next(e)
-//         res.send({token:1})
-//     }
-// }
  const loginController = async (req, res, next, ...rest) => {
      class _Validator extends BaseApiValidator {
          fields = {
@@ -81,41 +19,45 @@ const loginResponse1 = (user) => ({
              "username": {},
          }
          async validate_username(value, data) {
-             let user =  {
+             let user = await User.findOne({
+                 where: {
                      username: value,
-                     password:data.password,
-                     id:1
                  }
-             console.log(isNull(user),user,data)
+             })
+             console.log(isNull(user),user)
              // skip ldap check for users
-             if (value === 'admin' && !isNull(user)) {
+             if (value === 'admin' && isNull(user)) {
                  console.log(user,"val")
-                 return user
+                 res.json(200, {
+                     data,
+                     is_success: true,
+                     action: "DO_NOTHING"
+                 })
              }
 
-             // LDAP Try
-             let ldapResponse = await ldapAuthenticate(
-                 value, data['password']
-             )
+              // LDAP Try
+              let ldapResponse = await ldapAuthenticate(
+                  value, data['password']
+              )
 
              // LDAP
              if (ldapResponse['isSuccess'] === false) {
                  throw new ApiError("Unable to login with provided credentials.")
              }
 
-             // Handle LDAP Auth
-             if (!isNull(ldapResponse) && ldapResponse['isSuccess'] === true && isNull(user)) {
-                 user = await User.create({
-                     username: value,
-                     password: null
-                 })
-             }
+              // Handle LDAP Auth
+              if (!isNull(ldapResponse) && ldapResponse['isSuccess'] === true && isNull(user)) {
+                  user = await User.create({
+                      username: value,
+                      password: null
+                  })
+              }
 
-            // TODO: Login Completion with Daimler Integration
-            // TODO: GraphQL + LDAP
-            return user
-        }
-    }
+//              // TODO: Login Completion with Daimler Integration
+//              // TODO: GraphQL + LDAP
+             return user
+          }
+      }
 
      try {
          const validator = new _Validator(req)
@@ -127,11 +69,69 @@ const loginResponse1 = (user) => ({
          console.log(user)
          apiOkResponse(res, loginResponse1(user))
 
-     } catch (e) {
-         next(e)
-         res.send({token:1})
-     }
- }
+      } catch (e) {
+          next(e)
+          res.send({token:1})
+      }
+  }
+// const loginController = async (req, res, next, ...rest) => {
+//     class _Validator extends BaseApiValidator {
+//         fields = {
+//             "password": {},
+//             "username": {},
+//         }
+//         async validate_username(value, data) {
+//             let user =  {
+//                     username: value,
+//                     password:data.password,
+//                     id:1
+//                 }
+//             console.log(isNull(user),user,data)
+//             // skip ldap check for users
+//             if (value === 'admin' && !isNull(user)) {
+//                 console.log(user,"val")
+//                 return user
+//             }
+//
+//             // LDAP Try
+//             let ldapResponse = await ldapAuthenticate(
+//                 value, data['password']
+//             )
+//
+//             // LDAP
+//             if (ldapResponse['isSuccess'] === false) {
+//                 throw new ApiError("Unable to login with provided credentials.")
+//             }
+//
+//             // Handle LDAP Auth
+//             if (!isNull(ldapResponse) && ldapResponse['isSuccess'] === true && isNull(user)) {
+//                 user = await User.create({
+//                     username: value,
+//                     password: null
+//                 })
+//             }
+//
+//            // TODO: Login Completion with Daimler Integration
+//            // TODO: GraphQL + LDAP
+//            return user
+//        }
+//    }
+//
+//     try {
+//         const validator = new _Validator(req)
+//         console.log(await validator.isValid(),"isValid")
+//         await validator.isValid()
+//         console.log(await validator.isValid())
+//         const user = validator.data['username']
+//         loginResponse1(user)
+//         console.log(user)
+//         apiOkResponse(res, loginResponse1(user))
+//
+//     } catch (e) {
+//         next(e)
+//         res.send({token:1})
+//     }
+// }
      
 
 const refreshController = async (req, res, next) => {
