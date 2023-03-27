51cbc9e2628ff41df770a0c84d694b917cf42efa
chandranfullstack <chandran@fulgid.in>
2023-03-27 15:48:58 +0530

    tbale

--- dta_cyber_be_v2-assesment/src/config.js
+++ dta_cyber_be_v2-assesment/src/config.js
@@ -16,8 +16,8 @@ const envVariables = {
 } = process.env
 
 //const conString =new Sequelize( "postgres://postgres:Ravi@28022001r@localhost:5432/postgres")
-//const conString =new Sequelize("postgres://chan:Dp4djcq679AmS6jwcFhnxDnb3tqVhcwY@dpg-cgglmakeoogqfc3940j0-a/postgresdatabase_0rn9",{
-const conString=new Sequelize("postgres://chan:Dp4djcq679AmS6jwcFhnxDnb3tqVhcwY@dpg-cgglmakeoogqfc3940j0-a.oregon-postgres.render.com/postgresdatabase_0rn9",{
+const conString =new Sequelize("postgres://chan:Dp4djcq679AmS6jwcFhnxDnb3tqVhcwY@dpg-cgglmakeoogqfc3940j0-a/postgresdatabase_0rn9",{
+//const conString=new Sequelize("postgres://chan:Dp4djcq679AmS6jwcFhnxDnb3tqVhcwY@dpg-cgglmakeoogqfc3940j0-a.oregon-postgres.render.com/postgres",{
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
@@ -30,10 +30,12 @@ const conString=new Sequelize("postgres://chan:Dp4djcq679AmS6jwcFhnxDnb3tqVhcwY@
 const dbClient = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD, {
+    process.env.DB_PASSWORD, 
+    {
         host: process.env.DB_HOST,
         dialect: "postgres",
         logging:false,
+        ssl:{require:true}
     }
     // TODO: Read & Write Replication
     // Ref: https://sequelize.org/docs/v6/other-topics/read-replication/
